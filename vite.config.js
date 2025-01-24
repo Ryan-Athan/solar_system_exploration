81878d0eb85bb7e521174f1516d1c2124c0992f0
Thar Nyi Phyo <konyi.tharnyiphyo@gmail.com>
2025-01-24 14:15:45 +0630

    Update Init

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    publicDir: './public',  // Serve assets from the root `public` folder
+    publicDir: 'public',  // Serve assets from the root `public` folder
     root: '.',              // Root directory remains the project root
     build: {
         outDir: './dist',   // Output build files in the `dist` folder
